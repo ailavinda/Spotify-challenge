3ff491efad835c0fb5618735b030cefce281e4bf
ailavinda@gmail.com <ailavinda@gmail.com>
2018-06-14 07:20:19 -0400

    Spotify Questions

---
+++ question_1.js
@@ -0,0 +1,29 @@
+// Question 1 -- sortByStrings(s,t): 
+// Sort the letters in the string s by the order they occur in the string t. 
+// You can assume t will not have repetitive characters. 
+// For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". 
+// For s = "good"    and t = "odg",      the output should be sortByString(s, t) = "oodg".
+
+const sortByString = (s, t) => {
+
+  return t.split('')
+          .map( elT => {
+            return s.split('')
+                   .reduce( (acc, elS, k) => {
+                      return (( elS === elT ) ? acc.concat(k) : acc)
+                   }, []);
+          })
+          .reduce(( acc, elI, i ) => {
+            return acc.concat(elI
+                              .map( (el, l) => {
+                                return s.split('')[el];
+                              }));
+          },[]).join('');
+
+
+}
+
+console.log(sortByString('weather', 'therapyw')); // Should be ===> "theeraw"...
+console.log(sortByString('good', 'odg'));         // Should be ===> "oodg"...
+
+
